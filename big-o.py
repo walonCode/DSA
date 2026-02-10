aaf828c7817597f181502056f223cbb0fec71de8
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-10 20:41:44 +0000

    add some function to understand big o notation

--- big-o.py
+++ big-o.py
@@ -32,3 +32,36 @@ brand_name = "cosmo"
 
 average = get_avg_brand_followers(all_handles, brand_name)
 print(f"Average is {average:.2f}")
+
+
+def binary_search(target:int, arr:list[int]) -> bool:
+    arr = sorted(arr)
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        median = (low + high) // 2
+        if arr[median] == target:
+            return True
+        elif arr[median] < target:
+            low = median + 1
+        else:
+            high = median - 1
+    return False
+
+print(binary_search(10, [i for i in range(200)])) 
+
+def count_name(list_of_list:list[list[str]], target:str) -> int :
+    count = 0
+    for values in list_of_list:
+        for name in values:
+            if name == target:
+                count += 1
+    
+    return count
+
+all_names:list[list[str]] = [
+    ["walon", "cosmogirl", "billjane321"],
+    ["cosmokiller", "gr8", "cosmojane3"],
+    ["walon", "walon"]
+]    
+print(count_name(all_names,"walon"))    
